937145353dfa603b1ee3a95b445fb75f958bd6ac
Fogelman <ddfogelman@gmail.com>
2020-07-01 11:41:40 -0300

    ADD scraped documents

--- src/emitters/pages.js
+++ src/emitters/pages.js
@@ -9,11 +9,11 @@ async function run() {
   let connection = await amqp.connect(process.env.RABBITMQ_URI);
   let channel = await connection.createConfirmChannel();
 
-  const total = 49942;
+  const total = 27709;
   const rpp = 50;
 
   for (var i = 0; i < Math.ceil(total / rpp); i++) {
-    var obj = { href: "/bdsf/handle/id/44/browse", rpp, offset: i * rpp };
+    var obj = { href: "/bdsf/handle/id/30/browse", rpp, offset: i * rpp };
     await Emit.publish(channel, {
       exchange: "processing",
       key: "pages",
